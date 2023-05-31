c4e6fb3a59b8328e86c32c875c4c6e7b48324966
Apoorva Chauhan <47474447+apoorvacha@users.noreply.github.com>
2023-05-30 20:06:41 -0400

    Project files added

---
+++ App.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Button, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/navigation/HomeScreen";
+import BottomTab from "./src/navigation/BottomTab";
+import MessageScreen from "./src/navigation/MessageScreen";
+import EditProfile from "./src/navigation/EditProfile";
+import Comment from "./src/views/Comment/Comment";
+import Story from "./src/views/Story/Story";
+
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      {/* <Stack.Screen name="Login" component={Login} /> */}
+
+      <Stack.Screen name="BottomTab" component={BottomTab} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="MessageScreen" component={MessageScreen} />
+      <Stack.Screen name="EditProfile" component={EditProfile} />
+      <Stack.Screen name="Comment" component={Comment} />
+      <Stack.Screen
+        name="Story"
+        component={Story}
+        options={{
+          headerShown: false,
+          presentation: "modal",
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
